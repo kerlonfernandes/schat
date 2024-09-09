4d9c370e24eec1f2dc115dc6a38d9f758d2d5bb0
kerlonfernandes <kerlon1221@gmail.com>
2024-09-09 11:58:00 -0300

    chat update: connected users

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     
+    const onlineUsersDiv = document.querySelector(".online-users");
+
+
     const userData = JSON.parse(localStorage.getItem('user')) || {};
 
     if (!userData.name) {
@@ -8,23 +11,39 @@ document.addEventListener('DOMContentLoaded', () => {
         localStorage.setItem('user', JSON.stringify(userData));
     }
 
+
+    const quitBtn = document.querySelector(".quit");
+
+    quitBtn.addEventListener('click', () => {
+        localStorage.clear();
+        window.location.href = "./"
+    });
+
+
     let socket = io('https://0007-2804-3e60-4cf-3800-9a3c-576c-833b-a4c5.ngrok-free.app');
     
     const messagesDiv = document.querySelector('.messages');
 
+    function scrollToBottom() {
+        messagesDiv.scrollTop = messagesDiv.scrollHeight;
+    }
+
+    socket.on("userQtd", (usersQtd) => {
+        onlineUsersDiv.textContent = usersQtd;
+    });
+    
     socket.on('connect', () => {
-        
+
         socket.emit('user_data', userData.name);
 
         socket.on("user_entry", (user) => {
             messagesDiv.innerHTML += `<p><strong>${user} Entrou na sala.</strong></p>`;
-            window.scrollTo(0, document.body.scrollHeight);
+            scrollToBottom();
         });
 
         socket.on('receiveMessage', (data) => {
             messagesDiv.innerHTML += `<p><strong>${data.author}:</strong> ${data.message.slice(0, 300)}</p>`;
-            window.scrollTo(0, document.body.scrollHeight);
-        
+            scrollToBottom();
         });
        
     });
@@ -35,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let author = userData.name;
         let message = document.querySelector("#message").value.trim();
 
-        if(message == "/change-name") {
+        if(message === "/change-name") {
             userData.name = window.prompt("Coloca seu nome aí po");
         
             localStorage.setItem('user', JSON.stringify(userData));
@@ -46,10 +65,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 author: author,
                 message: message
             };
-            window.scrollTo(0, document.body.scrollHeight);
             socket.emit('sendMessage', messageObj);
+            document.querySelector("#message").value = "";
         }
 
-        document.querySelector("#message").value = "";
+        scrollToBottom();
     });
 });
--- src/socket/socketEvents.js
+++ src/socket/socketEvents.js
@@ -1,6 +1,13 @@
 module.exports = (io) => {
+
+    let onlineUsers = 0; 
+
     io.on('connection', (socket) => {
         console.log('A user connected:', socket.id);
+
+        onlineUsers+=1;
+
+        io.emit('userQtd', onlineUsers);
 
         socket.on('user_data', (data) => {
             io.emit("user_entry", data);
@@ -11,6 +18,8 @@ module.exports = (io) => {
         });
 
         socket.on('disconnect', () => {
+            onlineUsers-=1;
+            io.emit('userQtd', onlineUsers);
             console.log('User disconnected:', socket.id);
         });
     });
