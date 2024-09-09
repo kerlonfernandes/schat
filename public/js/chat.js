15be47b646ba91adec7d78b9e254fdf54e10b5fd
kerlonfernandes <kerlon1221@gmail.com>
2024-09-09 15:22:52 -0300

    comandos

---
+++ public/js/chat.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let input = document.querySelector("#message");
+    
+    input.addEventListener("input", (e) => {
+        let text = input.value.trim();
+        
+        let data = text.split(' '); 
+        let command = data[0];
+        
+        if (command === "/link" && data[1] && data[1].startsWith("https://") && data[2]) {
+            let linkHTML = `<a href="${data[1]}" target="_blank">${data[2]}</a>`;
+            
+            input.value = linkHTML;
+        }
+    });
+});
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (!userData.name) {
         userData.name = window.prompt("Coloca seu nome aí po");
+        userData.profilePic = window.prompt("Quer colocar uma imagem de perfil? (a imagem deve estar hospedada)");
         
         localStorage.setItem('user', JSON.stringify(userData));
     }
@@ -42,8 +43,19 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
         socket.on('receiveMessage', (data) => {
-            messagesDiv.innerHTML += `<p><strong>${data.author}:</strong> ${data.message.slice(0, 300)}</p>`;
-            scrollToBottom();
+            messagesDiv.innerHTML += `
+            <div class="card message-card m-4">
+                <div class="card-body d-flex align-items-center">
+                    ${data.profilePic ? `<img src="${data.profilePic}" alt="Profile Picture" class="profile-pic" height="100" width="100" style="border-radius:50%;">` : ''}
+                    <div class="username ms-3">${data.author}</div>
+                </div>
+                <hr>
+                <div class="card-body">
+                    <p>${data.message.slice(0, 300)}</p>
+                </div>
+            </div>
+        `;
+         scrollToBottom();
         });
        
     });
@@ -53,6 +65,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         let author = userData.name;
         let message = document.querySelector("#message").value.trim();
+        let profilePic = userData.profilePic;
 
         if(message === "/change-name") {
             userData.name = window.prompt("Coloca seu nome aí po");
@@ -63,6 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (author.length && message.length) {
             let messageObj = {
                 author: author,
+                profilePic: profilePic,
                 message: message
             };
             socket.emit('sendMessage', messageObj);
